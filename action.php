5de515eb1b4f9c62d6ff21908d295c944362cd9e
Anant Bhasin <BhasinAnant@gmail.com>
2018-08-18 16:08:18 +0530

    Boilerplate V1
    Initial commit

---
+++ action.php
@@ -0,0 +1,22 @@
+<?php
+include 'config/db.conf.php';
+include 'config/meta.conf.php';
+include 'config/sys.conf.php';
+session_start();
+if(isset($_GET['plug']) && isset($_GET['a']))
+{
+	$filetoInc = "plugins/".$_GET['plug']."/back/".$_GET['a'] . ".php";
+}
+else if(isset($_GET['a']))
+{
+	$filetoInc = 'backend/'.$_GET['a'].'.php';
+}
+if(file_exists($filetoInc))
+{
+	include $filetoInc;
+}
+else
+{
+	header("Location: " . $GLOBALS['host'] . "Errors/404");
+}
+?>
